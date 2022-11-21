cbc02646912fb73e4dd4e9514a848c41385a92a3
AndreCosta201905916 <up201905916@up.pt>
2022-11-21 14:11:29 +0000

    recursion and lists

--- Prolog/ficha2.pl
+++ Prolog/ficha2.pl
@@ -88,10 +88,8 @@ fibonacci(N,F):- N > 1,
 
 %# d
 
-isPrime(1,1).
-isPrime(X):- N>1,
-             N1 is N-1,
-             isPrime()
+%isPrime(1,1).
+%isPrime(X):- N>1, N1 is N-1, isPrime().
 
 %# Tail recursion
 %# 
@@ -102,3 +100,90 @@ isPrime(X):- N>1,
 %#                            NAcc is Acc * N,
 %#                            N1 is N - 1,
 %#                            fact(N1, NAcc, F).
+
+
+%# 2.
+
+%# 3.
+
+%# 4.
+%# a) True
+%# b) False, tail must be a list
+%# c) True, [a | [b, c, d]] = [a | [b | [c, d]]] = [a | [b | [c | d]]]
+%# d) True
+%# e) True, [H|T] both can be just one element
+%# f) True, Tail can be []
+%# g) False, [H|T] H needs to be a element
+%# h) True, T can be a list inside a list
+%# i) True, T can be a variable
+%# j) True, if Inst = gram && LEIC = feup
+%# k) True, Head can be more than one element
+%# l) True, if One = leic && (Two | Tail) = Rest
+
+%# 5.
+
+%# c)
+list_prod([H], H).
+list_prod([H | T], P):- list_prod(T, P1),
+                        P is H*P1.
+
+%# d) 
+inner_prod([], [], 0).
+inner_prod([H1|T1], [H2|T2], Res):- inner_prod(T1, T2, R),
+                                    Res is H1*H2 + R.
+
+%# e)
+count(_, [], 0).
+count(X, [X|T], N):- count(X, T, N1),
+                     N is N1+1.
+count(X, [_|T], N):- count(X, T, N).
+
+%# 6.
+
+%# b)
+del_one(_, [], []).
+del_one(H, [H|T], T).
+del_one(X, [H|T], Res):- X\=H, del_one(X, T, Res1),
+                         Res = [H|Res1].
+
+%# c)
+del_all(_, [], []).
+del_all(X, [X|T], Res):- del_all(X, T, Res).
+del_all(X, [H|T], Res):- X\=H, del_all(X, T, Res1),
+                         Res = [H|Res1].
+
+%# d)
+del_all_list(_, [], []).
+del_all_list([H|[]], L2, Res):- del_all(H, L2, Res).
+del_all_list([H|T], L2, Res):- del_all(H, L2, Res1), 
+                               del_all_list(T, Res1, Res).
+
+%# g)
+replicate(0, _, []).
+replicate(N, Elem, Res):- N1 is N-1, 
+                          replicate(N1, Elem, Res1),
+                          Res = [Elem|Res1].
+
+%# h)
+intersperse(_, [H|[]], [H]).
+intersperse(Elem, [H|T], Res):- intersperse(Elem, T, Res1),
+                                Res = [H,Elem|Res1].
+
+%# i)
+insert_elem(0, [H|T], Elem, [Elem, H |T]).
+insert_elem(Pos, [H|T], Elem, Res):- Pos1 is Pos-1,
+                                     insert_elem(Pos1, T, Elem, Res1),
+                                     Res = [H|Res1].
+
+%# j)
+%delet_elem()
+
+%# 7.
+
+%# 8.
+
+%# 9.
+
+%# 10.
+
+%# 11.
